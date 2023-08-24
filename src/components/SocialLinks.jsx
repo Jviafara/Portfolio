38dd24631a2f8c7057e4837d20a074e872abebf0
Jviafara <jealvito@gmail.com>
2023-08-23 23:50:50 -0500

    update CV

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -44,7 +44,7 @@ const SocialLinks = () => {
                     Resume <BsFillPersonLinesFill size={30} />
                 </>
             ),
-            href: '/Jesús_Viafara_CV.pdf',
+            href: '/Jesús_Viafara.pdf',
             style: 'rounded-br-md',
             download: true,
         },
